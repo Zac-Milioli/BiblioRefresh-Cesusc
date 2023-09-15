5593cf1d9b17cf942c1267840fd9865b72254349
Zac-Milioli <zacmilioli@gmail.com>
2023-09-15 14:48:03 -0300

    Alteração para receber os valores de biblioSaida + leitura de sheets

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # import os
 # os.system('pip install pandas')
 # os.system('pip install numpy')
+# os.system('pip install xlrd')
 
 from glob import glob
 import pandas as pd
@@ -19,24 +20,35 @@ glob_retiradas = glob(path_retiradas)
 print(separators)
 print(f"\n\tEncontrada planilha principal {df_principal_em_path}\n")
 print(f"\n\tEncontradas planilhas de retirada em\n{glob_retiradas}\n")
+print(separators)
 
-df_principal = pd.read_excel(df_principal_em_path)
+df_principal = pd.read_excel(df_principal_em_path, sheet_name='Total Livros')
 
 for retirada in glob_retiradas:
-    remover = pd.read_excel(retirada)
-    print(separators)
-    print(f"\n\tLeu {retirada}\n")
-    if 'Tombo' in remover.columns:
-        print("\n\tEste dataframe possui tombos, verificando cada um...\n")
+    if 'biblioSaida' in retirada:
+        remover = pd.read_excel(retirada, sheet_name='Total Doados')
+        print(separators)
+        print(f"\n\tLeu {retirada}\n")
         for tombo in remover['Tombo']:
             if tombo != np.nan:
-                print(f"- Verificando existência de {tombo}\tna planilha principal", end='\r')
+                print(f"- Verificando existência de \t{tombo}\tna planilha principal", end='\r')
                 df_principal.loc[df_principal['Tombo'] == tombo] = np.nan
     else:
-        print(f'\n\tA planilha {retirada} não possui tombos, será ignorada')
+        remover = pd.read_excel(retirada)
+        print(separators)
+        print(f"\n\tLeu {retirada}\n")
+        if 'Tombo' in remover.columns:
+            print("\n\tEste dataframe possui tombos, verificando cada um...\n")
+            for tombo in remover['Tombo']:
+                if tombo != np.nan:
+                    print(f"- Verificando existência de \t{tombo}\tna planilha principal", end='\r')
+                    df_principal.loc[df_principal['Tombo'] == tombo] = np.nan
+        else:
+            print(f'\n\tA planilha {retirada} não possui tombos, será ignorada')
+            print(separators)
 
 print(separators)
 df_principal.dropna(axis=0, inplace=True)
-df_principal.to_excel(df_principal_em_path, na_rep='')
+df_principal.to_excel(df_principal_em_path, sheet_name='Total Livros')
 print(f"\n- Arquivo {df_principal_em_path} atualizado\n")
 print(separators)
